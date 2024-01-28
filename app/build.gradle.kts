9c1ce63e9dfe12b51e09198fda88f2b60cf0344c
Mehrdad S.F. <meddisommer@gmail.com>
2024-01-29 01:11:51 +0330

    Completed the project - final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.digikalasample"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.digikalasample"
         minSdk = 28
