6c2cc6b091fb610825acc061d815d46152fa318f
Mehrdad S.F. <meddisommer@gmail.com>
2024-02-10 21:14:17 +0330

    Update the project commit

--- app/src/main/java/com/example/digikalasample/MainActivity.kt
+++ app/src/main/java/com/example/digikalasample/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.digikalasample
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.digikalasample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Set the requested orientation to portrait.
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
